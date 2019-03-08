d80a9edfa7c94b5e684f2d1d775ef289f80af6b5
hyundai <dwayne.shim@gmail.com>
2019-03-08 15:31:53 +0900

    developing es-plugin ...

--- core/src/main/java/klay/core/Klay.java
+++ core/src/main/java/klay/core/Klay.java
@@ -25,8 +25,9 @@ import java.nio.file.Paths;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
-// TODO : 1. Testcase 작성하기
+// TODO : 1. ES plugin 개발하기
 // TODO : 2. FWDAndEmissionMixRule 만들기 (FWD 사전 + Emission사전)
+// TODO : 3. Testcase 작성하기
 public class Klay {
 
     private final ChainedTokenizationRule tokenizationRule;
---
+++ es-plugin/src/main/java/klay/es/plugin/analysis/AnalysisKlayPlugin.java
@@ -0,0 +1,7 @@
+package klay.es.plugin.analysis;
+
+import org.elasticsearch.plugins.AnalysisPlugin;
+import org.elasticsearch.plugins.Plugin;
+
+public class AnalysisKlayPlugin extends Plugin implements AnalysisPlugin {
+}
